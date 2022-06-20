4733e65da04671cd4df590ca1a0fd2f758d128a3
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2022-06-20 16:43:50 -0300

    IntroToCS-HW

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,28 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let arrNum = Array.from(String(num), Number);
+  let convertido = 0;
+  for (let i = 0; i < arrNum.length; i++) {
+      convertido = convertido * 2 + arrNum[i];
+     }
+  return convertido
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let convertido = []
+  let aConvertir 
+  do {
+    aConvertir = num % 2;
+    convertido.unshift([aConvertir]);
+      if (num % 2 > 0) {
+      num = num - 1;
+      }
+    num = num / 2;
+  }
+  while (num / 2 > 0);
+  return convertido.join('');
 }
 
 
