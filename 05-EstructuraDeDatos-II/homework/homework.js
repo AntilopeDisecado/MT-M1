db15e704df07491b0a46a5112a675601d4cb690e
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2022-06-28 00:58:17 -0300

    Homework 05

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -3,17 +3,73 @@
 /*
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
-  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
-  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una 
+    lista de un solo nodo y de una lista vacía);
+  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser 
+  un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, 
+  buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
   Ejemplo: 
   search(3) busca un nodo cuyo valor sea 3;
-  search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
+  search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, 
+  busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+function LinkedList() {
+    this.head = null;
+ }
 
-function Node(value) {}
+function Node(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+let list = new LinkedList ();
+
+LinkedList.prototype.add = function (data) {
+let node = new Node(data);
+let current = this.head;
+if (!current) {
+  this.head = node;
+  return node;
+}
+while (current.next) {
+  current = current.next;
+}
+current.next = node;
+return node;
+}
+
+LinkedList.prototype.remove = function () {
+let current = this.head;
+if (!current) return null;
+if (!current.next) {
+let aux = current.value;
+this.head = null;
+return aux;
+}
+while (current.next.next) {
+  current = current.next;
+}
+let aux = current.next.value
+current.next = null;
+return aux;
+}
+
+LinkedList.prototype.search = function (value, cb) {
+  let current = this.head;
+  if (!current) return null;
+  while (current) {
+   if (current.value === value) return current.value;
+   else if (typeof value === 'function') {
+    if(value(current.value) === true) {
+      return current.value
+    }
+  }
+  current = current.next;
+  }
+  return null;
+}
 
 /*
 Implementar la clase HashTable.
@@ -30,7 +86,39 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+function HashTable() {
+  this.numBuckets = 35;
+  this.buckets = [];
+}
+
+HashTable.prototype.hash = function(key) {
+  let aux = 0
+  for (let i = 0; i < key.length; i++)
+  aux += key.charCodeAt(i);
+  return aux % this.numBuckets;
+}
+
+HashTable.prototype.set = function (key, value) {
+  if (typeof key !== 'string') {
+    throw new TypeError ('Keys must be strings');
+  }
+  let pos = this.hash(key);
+  if (this.buckets[pos] === undefined) {
+    this.buckets[pos] = {}
+  }
+  this.buckets[pos][key] = value;
+  }
+
+HashTable.prototype.get = function (key) {
+  let pos = this.hash(key);
+  return this.buckets[pos][key]
+}
+
+HashTable.prototype.hasKey = function (key) {
+  let haskey = this.get(key);
+  if (haskey) return true;
+  else return false;
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
